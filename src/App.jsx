bfbda168e269870e3de1fefacec31f4498077b41
manikpote <potemanik123@gmail.com>
2024-12-01 13:13:13 +0545

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
-import TodoList from "./components/todoList";
-import { Login } from "./components/login";
+import { Login } from "./components/todoList/login";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
+import TodoList from "./components/todoList/todoList";
+import { Popup } from "./components/reuseableComponents/popup";
+import { RangeSlider } from "./components/RangeSlider/rangeSlider";
 
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Login />} />
+          <Route path="/popup" element={<Popup />} />
+          <Route path="/password" element={<RangeSlider />} />
           <Route path="/todo/:userId" element={<TodoList />} />
         </Routes>
       </BrowserRouter>
